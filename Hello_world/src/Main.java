e34dd7bb08a1ddbe4ef1a1bff03f768bfe94010f
Leva Oops <levaoop@gmail.com>
2026-09-22 09:47:23 +0500

    Изменение в ветке

--- Hello_world/src/Main.java
+++ Hello_world/src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello people");
         System.out.println('Hello world');
+        int num;
+        boolean num1;
+        String nam;
+        nam = "home";
     }
 }
